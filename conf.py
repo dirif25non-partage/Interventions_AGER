2b08e57e1b93ba9f1319127a16cf726d8f672b34
ExploitIdF <141207765+ExploitIdF@users.noreply.github.com>
2024-04-04 07:58:52 +0200

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,28 @@
+project = 'Interventions sur les réseaux du DETT'
+copyright = '2024-04, on'
+author = 'on'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'furo'
+html_css_files = ['custom.css']
+html_favicon = 'https://storage.googleapis.com/idf-congestion/favicon.ico'
+html_static_path = ['_static']
+html_show_copyright = False
+html_show_sphinx = False
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "logo.jpg",
+    "dark_logo": "logo.jpg",   
+}
